13087be1b50541dec62d70271275b9b770da3ab6
ExIntercept <aryansrivastava1004@gmail.com>
2024-01-31 21:32:43 +0530

    App Functionality Complete

--- app/src/main/java/com/aryans/midnightmirage/MainActivity.kt
+++ app/src/main/java/com/aryans/midnightmirage/MainActivity.kt
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.content.Intent
+import android.widget.ImageButton
 
-private const val INITITAL_COUNT = 0
+private const val INITIAL_COUNT = 0
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bCounter: Button
     private lateinit var tvCounter: TextView
+    private lateinit var bNextPage: ImageButton
+    private lateinit var tvFunny: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,16 +23,35 @@ class MainActivity : AppCompatActivity() {
 
         bCounter = findViewById(R.id.bCounter)
         tvCounter = findViewById(R.id.tvCounter)
+        bNextPage = findViewById(R.id.bNextPage)
+        tvFunny = findViewById(R.id.tvFunny)
 
-        tvCounter.text = "$INITITAL_COUNT"
+        tvCounter.text = "$INITIAL_COUNT"
 
         bCounter.setOnClickListener {
             val Count = tvCounter.text.toString().toInt()
             val nCount = Count + 1
             tvCounter.text = "$nCount"
+            nice()
+        }
+
+        bNextPage.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
 
 
-
     }
+
+    private fun nice() {
+        if (tvCounter.text.toString().toInt() == 69 || tvCounter.text.toString()
+                .toIntOrNull() == 420
+        ) {
+            tvFunny.text = "Nice LOL!"
+            return
+        }
+        tvFunny.text = ""
+    }
+
+
 }
--- app/src/main/java/com/aryans/midnightmirage/SecondActivity.kt
+++ app/src/main/java/com/aryans/midnightmirage/SecondActivity.kt
@@ -2,11 +2,81 @@ package com.aryans.midnightmirage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.widget.ImageButton
+import com.aryans.midnightmirage.R.*
+import android.content.Intent
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-class SecondActivity: AppCompatActivity() {
+const val INITIAL_VALUE = 100
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var bPreviousPage: ImageButton
+    private lateinit var bDownCounter: Button
+    private lateinit var etDownCounter: EditText
+    private lateinit var tvCounter2: TextView
+    private lateinit var tvFunny2: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        setContentView(layout.activity_second)
+
+        bPreviousPage = findViewById(id.bPreviousPage)
+        bDownCounter = findViewById(id.bDownCounter)
+        etDownCounter = findViewById(id.etDownCounter)
+        tvCounter2 = findViewById(id.tvCounter2)
+        tvFunny2 = findViewById(id.tvFunny2)
+
+        tvCounter2.text = "$INITIAL_VALUE"
+
+        bPreviousPage.setOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        etDownCounter.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                updateCounter()
+            }
+
+        })
+
+        bDownCounter.setOnClickListener {
+            val counter = tvCounter2.text.toString().toInt() - 1
+            tvCounter2.text = "$counter"
+            nice()
+        }
+
     }
+
+
+    private fun updateCounter() {
+        if (etDownCounter.text.isEmpty()) {
+            tvCounter2.text = "$INITIAL_VALUE"
+            return
+        }
+        tvCounter2.text = etDownCounter.text.toString()
+        nice()
+    }
+
+    private fun nice() {
+        if (tvCounter2.text.toString().toInt() == 69 || tvCounter2.text.toString()
+                .toIntOrNull() == 420
+        ) {
+            tvFunny2.text = "Nice LOL!"
+            return
+        }
+        tvFunny2.text = ""
+    }
+
 }
+
+
